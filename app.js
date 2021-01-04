2786ace42924fc2131d06e8fe3e15f39019b7132
hthanhdev <63234227+hthanhdev@users.noreply.github.com>
2021-01-04 14:39:53 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const app = express();
+
+require("dotenv").config();
+
+const PORT = process.env.PORT || 3100;
+const environment = process.env.NODE_ENV || "development";
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.send(`Listening on Port ${PORT} / at ${environment} Env `);
+});
+
+app.get("/ping", (req, res) => {
+  res.send({ status: "Ok" });
+});
+// CORS ALL ACCESS
+app.disable("x-powered-by");
+
+require('./router')(app);
+
+app.listen(PORT, () => {
+  console.info(`[ApiServer] Listening on Port ${PORT} / at ${environment} Env`);
+});
+
+module.exports = app;
